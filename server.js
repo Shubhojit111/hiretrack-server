fbdad05d3af9f64cfee028ea6004862003d27507
Shubhojit Deb <shubhojityes@gmail.com>
2026-03-01 10:35:21 +0530

    NODE_ENV setup for production

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
-const express = require("express");
-const cors = require("cors");
-const authRoutes = require("./routes/Auth/authRoutes");
-const jobRoutes = require("./routes/jobRoutes");
-const cookieParser = require("cookie-parser");
-const connectDB = require("./config/db");
-const errorMiddleware = require("./middleware/errorMiddleware");
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/Auth/authRoutes');
+const jobRoutes = require("./routes/jobRoutes")
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+const errorMiddleware = require('./middleware/errorMiddleware');
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
 app.set("trust proxy", 1);
 // Middleware
-app.use(
-  cors({
-    origin: [
-        "https://hiretrack-shubhojit.vercel.app",
-         "http://localhost:5173",
-        //  "http://localhost:3000"
-        ],
+app.use(cors({
+    origin: "https://hiretrack-shubhojit.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,17 +26,17 @@ app.use(cookieParser());
 connectDB();
 
 // Routes Placeholder
-app.get("/", (req, res) => {
-  res.send("HireTrack API is running...");
+app.get('/', (req, res) => {
+    res.send('HireTrack API is running...');
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/job", jobRoutes);
+app.use("/api/auth",authRoutes);
+app.use("/api/job",jobRoutes);
 
-app.use(errorMiddleware);
+app.use(errorMiddleware)
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
+    console.log(`Server running on port http://localhost:${PORT}`);
 });
