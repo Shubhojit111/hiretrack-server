0efdf41d3fdb3e16b552775d26d9aa2d0238b0ab
Shubhojit Deb <shubhojityes@gmail.com>
2026-03-01 10:37:26 +0530

    NODE_ENV setup for production didnt fixed so rollback

--- server.js
+++ server.js
@@ -10,14 +10,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.set("trust proxy", 1);
+
 // Middleware
 app.use(cors({
-    origin: "https://hiretrack-shubhojit.vercel.app",
+    origin: [
+    "http://localhost:5173",
+    "https://hiretrack-shubhojit.vercel.app" 
+  ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  }));
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
